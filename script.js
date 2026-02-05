39cb465aeaae6f31b02a396833cb601f6e7db3e3
kina33800 <soufiane33800@gmail.com>
2026-02-05 16:07:11 +0100

    rolex

--- script.js
+++ script.js
@@ -50,3 +50,77 @@ setInterval(afficherHeure, 1000);
 
 document.getElementById('bouton').onclick = programmer;
 
+// Exercice 1 nombre paire
+
+// function paire (number) {
+//     return number %2 === 0
+// }
+// console.log (paire(10)) 
+// console.log (paire(29)) 
+// console.log (paire(52)) 
+
+// Exercice 2 palindrome
+
+// function palindrome (chaine) {
+//    return chaine === chaine.split('').reverse().join('');  
+// }
+
+// let mot = prompt ("ecrivez un mot");
+// if (palindrome(mot)) {
+//     alert ('Le mot est un palindrome');
+// }
+// else { 
+//     alert ("faux, veuillez entrer un nouveau mot");
+// }
+
+// console.log (palindrome("kayak"))
+// console.log (palindrome("bonbon"))
+// console.log (palindrome("bonjour"))
+
+// Exercice II feu tricolore
+
+// Variable pour savoir quel feu est allumé
+
+// Fonction pour allumer un feu et éteindre les autres
+
+function allumerFeu(couleur) {
+    // Éteindre tous les feux
+    document.getElementById('rouge').className = 'light';
+    document.getElementById('orange').className = 'light';
+    document.getElementById('vert').className = 'light';
+    
+    // Allumer le feu demandé
+    if (couleur == 'rouge') {
+        document.getElementById('rouge').className = 'light lightRed';
+    }
+    if (couleur == 'vert') {
+        document.getElementById('vert').className = 'light lightGreen';
+    }
+    if (couleur == 'orange') {
+        document.getElementById('orange').className = 'light lightOrange';
+    }
+}
+
+let compteur = 0;
+
+// changement de couleur
+function changerFeu() {
+    if (compteur == 0) {
+        allumerFeu('rouge');
+        compteur = 1;
+    }
+    else if (compteur == 1) {
+        allumerFeu('vert');
+        compteur = 2;
+    }
+    else {
+        allumerFeu('orange');
+        compteur = 0;
+    }
+}
+
+// Démarrer avec le rouge
+allumerFeu('rouge');
+
+// Toutes les 5 secondes
+setInterval(changerFeu, 5000);
