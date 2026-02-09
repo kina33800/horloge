052a0547909768a67134f7f67e78ed83da9d92d8
kina33800 <soufiane33800@gmail.com>
2026-02-09 16:03:42 +0100

    rpg

--- script.js
+++ script.js
@@ -125,6 +125,8 @@ allumerFeu('rouge');
 // Toutes les 5 secondes
 setInterval(changerFeu, 5000);
 
+// Exercice 3
+
 // Récupérer les éléments
 let modal = document.getElementById('ma-modal');
 let bouton = document.getElementById('bouton-modal');
@@ -152,3 +154,186 @@ window.onclick = function(event) {
         modal.style.display = 'none';
     }
 }
+
+// Exercice 4
+
+// class calculateur {
+//     constructor (valeurInitiale) {
+//        this.total = valeurInitiale; 
+//     }
+//     ajouter(nombre) {
+//         this.total += nombre;
+//     }
+//     afficher () {
+//         console.log(`le total actuel est : ${this.total}`);
+//     }
+// }
+
+// Classe pour calculer la somme
+class Calculateur {
+    constructor (valeurInitiale) {
+       this.total = valeurInitiale; 
+    }
+    ajouter(nombre) {
+        let total = 0;
+        for (let nombre of this.total) {
+            total += nombre;
+        }
+        return total;  
+    }
+    afficher () {
+        let finalSum = this.ajouter ();
+        console.log(`le total actuel est : ${finalSum}`);
+    }
+}
+
+let maListe = [5, 10, 15, 20];
+let autreListe = [100, 200, 50, 25];
+let monCalculateur = new Calculateur(maListe);
+let monCalculateur2 = new Calculateur(autreListe);
+
+
+monCalculateur.afficher(maListe);
+monCalculateur2.afficher(monCalculateur2);
+
+// Exercice 5
+
+// Classe pour les armes
+class Weapon {
+    constructor() {
+        this.fist = 'Poing';
+        this.saber = 'Sabre';
+        this.rifle = 'Fusil';
+    }
+    
+    afficher() {
+        console.log(`Les armes sont : ${this.fist}, ${this.saber} et ${this.rifle}`);
+    }
+}
+
+// Classe pour les sorts
+class Sort {
+    constructor() {
+        this.teleportation = 'Téléportation';
+        this.petrification = 'Pétrification';
+        this.regeneration = 'Régénération';
+    }
+    
+    afficher() {
+        console.log(`Les sorts sont : ${this.teleportation}, ${this.petrification} et ${this.regeneration}`);
+    }
+}
+
+// Utilisation
+const mesArmes = new Weapon();
+mesArmes.afficher();
+
+const mesSorts = new Sort();
+mesSorts.afficher();
+
+// Exercice 6
+
+// CLASSE PARENTE : Personnage
+
+class Personnage {
+    constructor(nom, sante, force, titre) {
+        this.nom = nom;
+        this.sante = sante;
+        this.force = force;
+        this.titre = titre;
+    }
+    
+    // afficher les informations
+    afficher() {
+        console.log(`Nom: ${this.nom}`);
+        console.log(`Titre: ${this.titre}`);
+        console.log(`Santé: ${this.sante} HP`);
+        console.log(`Force: ${this.force}`);
+    }
+}
+
+// CLASSE Guerrier
+class Guerrier extends Personnage {
+    constructor(nom, sante, force, armure, arme) {
+        super(nom, sante, force, 'Guerrier');
+        this.armure = armure;
+        this.arme = arme;
+    }
+    
+    // Méthode spécifique au Guerrier
+    afficher() {
+        super.afficher(); 
+        console.log(`Armure: ${this.armure}`);
+        console.log(`Arme: ${this.arme}`);
+    }
+}
+
+// CLASSE Mage
+
+class Mage extends Personnage {
+    constructor(nom, sante, force, mana, sort) {
+        super(nom, sante, force, 'Mage');
+        this.mana = mana;
+        this.sort = sort;
+    }
+    
+    afficher() {
+        super.afficher();
+        console.log(`Mana: ${this.mana} MP`);
+        console.log(`Sort principal: ${this.sort}`);
+    }
+}
+
+// CLASSE Archer
+
+class Archer extends Personnage {
+    constructor(nom, sante, force, agilite, typeArc) {
+        super(nom, sante, force, 'Archer');
+        this.agilite = agilite;
+        this.typeArc = typeArc;
+    }
+    
+    afficher() {
+        super.afficher();
+        console.log(`Agilité: ${this.agilite}`);
+        console.log(`Type d'arc: ${this.typeArc}`);
+    }
+}
+
+// CLASSE Voleur
+
+class Voleur extends Personnage {
+    constructor(nom, sante, force, discretion, armeSecrete) {
+        super(nom, sante, force, 'Voleur');
+        this.discretion = discretion;
+        this.armeSecrete = armeSecrete;
+    }
+    
+    afficher() {
+        super.afficher();
+        console.log(`Discrétion: ${this.discretion}`);
+        console.log(`Arme secrète: ${this.armeSecrete}`);
+    }
+}
+
+// CRÉATION DES PERSONNAGES
+
+console.log(' MES PERSONNAGES \n');
+
+// Créer un Guerrier
+const aragorn = new Guerrier('Guerrier', 150, 85, 'Armure lourde', 'Épée longue');
+aragorn.afficher();
+
+// Créer un Mage
+const gandalf = new Mage('Mage', 80, 40, 200, 'Boule de feu');
+gandalf.afficher();
+
+// Créer un Archer
+const legolas = new Archer('Archer', 100, 60, 95, 'Arc elfique');
+legolas.afficher();
+
+// Créer un Voleur
+const ezio = new Voleur('Voleur', 90, 55, 98, 'Lame cachée');
+ezio.afficher();
+
+console.log('\n Tous les personnages ont été créés !');
